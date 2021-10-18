f08693322e01ded04ca8a73dd752f8d2abc30d84
Graham Beaumont <grb011@student.usc.edu.au>
2021-10-18 22:11:06 +1000

    Updated Project

---
+++ app/src/main/java/com/bignerdranch/android/plainolnotes/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.bignerdranch.android.plainolnotes
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bignerdranch.android.plainolnotes.data.AppDataBase
+import com.bignerdranch.android.plainolnotes.data.NoteEntity
+import com.bignerdranch.android.plainolnotes.data.SampleDataProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val database = AppDataBase.getInstance(app)
+
+    val notesList = database?.noteDao()?.getAll()
+
+    fun addSampleData (){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                val sampleNotes = SampleDataProvider.getNotes()
+                database?.noteDao()?.insertAll(sampleNotes)
+            }
+        }
+    }
+
+    fun deleteNotes(selectedNotes: List<NoteEntity>) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                database?.noteDao()?.deleteNotes(selectedNotes)
+            }
+        }
+    }
+
+    fun deleteAllNotes() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                database?.noteDao()?.deleteAllNotes()
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/plainolnotes/data/NoteEntity.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.plainolnotes.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.bignerdranch.android.plainolnotes.NEW_NOTE_ID
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+
+@Entity(tableName = "notes")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var date: Date,
+    var text: String
+) : Parcelable {
+    constructor() : this(NEW_NOTE_ID, Date(), "")
+    constructor(date: Date, text: String) : this(NEW_NOTE_ID, date, text)
+}
